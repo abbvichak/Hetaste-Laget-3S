ab4f64c992005f6112474aa9bca8186a6ae0eacb
abbvichak <victor.hakonsen@abbgymnasiet.se>
2021-10-19 09:35:00 +0200

    Kopplade firebases databas tror jag

--- TempHum.js
+++ TempHum.js
@@ -1,69 +1,7 @@
-// function currentTime() {
-//     let date = new Date(); 
-//     let hh = date.getHours();
-//     let mm = date.getMinutes();
-//     let ss = date.getSeconds();
-//     let session = "AM";
-  
-//     if(hh == 0){
-//         hh = 12;
-//     }
-//     if(hh > 12){
-//         hh = hh - 12;
-//         session = "PM";
-//      }
-  
-//      hh = (hh < 10) ? "0" + hh : hh;
-//      mm = (mm < 10) ? "0" + mm : mm;
-//      ss = (ss < 10) ? "0" + ss : ss;
-      
-//      let time = hh + ":" + mm + ":" + ss + " " + session;
-  
-//     document.getElementById("clock").innerText = time; 
-//     let t = setTimeout(function(){ currentTime() }, 1000);
-//   }
-//   currentTime();
-
-//BUTTON TEST?
-
-// let btn = document.createElement("button");
-// btn.innerHTML = "Save";
-// btn.onclick = function () {
-// alert("Button is clicked");
-// };
-// document.body.appendChild(btn);
-
-//-
-
-//   JSC.Chart('chartDiv', {
-//     type: 'horizontal column',
-//     series: [
-//        {
-//           name:'Andy',
-//           points: [
-//              {x: 'Apples', y: 50},
-//              {x: 'Oranges', y: 32}
-//           ]
-//        },{
-//           name:'Anna',
-//           points: [
-//              {x: 'Apples', y: 30},
-//              {x: 'Oranges', y: 22}
-//           ]
-//        }
-//     ]
-//  });
+let database = firebase.database();
 
 
-//C TO F CONVERTER?
-
-// function convertToF(celsius) {
-//     return celsius * 9/5 + 32
-//   }
-  
-//   convertToF(30);
-
-  function Time() {
+function Time() {
     // Creating object of the Date class
     var date = new Date();
     // Get current hour
@@ -110,31 +48,6 @@
    }
    Time();
 
-   
-   var admin = require("firebase-admin");
-
-   var serviceAccount = require("path/to/serviceAccountKey.json");
-   
-   admin.initializeApp({
-     credential: admin.credential.cert(serviceAccount),
-     databaseURL: "https://hetaste-laget-s3-default-rtdb.europe-west1.firebasedatabase.app"
-   });  
-
-
-   import { initializeApp } from 'firebase/app';
-
-   const firebaseConfig = {
-    apiKey: "AIzaSyAfxNn3StYr8XoMJnCsk_6TZPnCVe5gyig",
-    authDomain: "hetaste-laget-s3.firebaseapp.com",
-    databaseURL: "https://hetaste-laget-s3-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "hetaste-laget-s3",
-    storageBucket: "hetaste-laget-s3.appspot.com",
-    messagingSenderId: "1063955437225",
-    appId: "1:1063955437225:web:b80891af14d7635aac7bb8",
-    measurementId: "G-W5MRV5H1S3"
-  };
-
-   const app = initializeApp(firebaseConfig);
 
 
 
